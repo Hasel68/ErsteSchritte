f3d6852ca67a49592cb6157f28b132c6bd01ea51
Hasel68 <140375078+Hasel68@users.noreply.github.com>
2023-08-07 20:09:27 +0200

    Add files via upload

--- src/Arrays.java
+++ src/Arrays.java
@@ -1,18 +1,15 @@
 
-public class ArrayUndFor {
+public class Arrays {
 
 	public static void main(String[] args) {
-        int[] asdf = new int[10];
-        int[] wasd = new int [5];
-        for(int zaehler = 0; zaehler < 5; zaehler++)
-        {
-        	asdf[zaehler] = zaehler + 5;
-        	wasd[zaehler] = asdf[zaehler] * 12;	
-        }
-        for(int i = 0; i < 10; i++)
-        {
-        	System.out.println(asdf[i] * wasd[i % 5]);   // Mit % kann man bis einschließlich 4 rechnen lassen. Die 5 geht in sich selber rein und wird 0. Es wird mit Resten gerechnet ( wie oft passt die 5 in die 3 = 0 Mahl.
-        }
+		int [] box = {5,34,2,9,7,99};
+		System.out.println("Gebe wert 2 aus: " + box[1]);
+		int laenge = box.length;
+		System.out.println("Laenge Array: " + laenge);
+		box[1] = 12;
+		System.out.println("Gebe wert 2 aus: " + box[1]);
+			
+
 	}
 
 }
